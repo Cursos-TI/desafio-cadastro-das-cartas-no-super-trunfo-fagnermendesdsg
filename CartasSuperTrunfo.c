1f18ad12f039a3eae5d887a3506171ab8b6d3822
fagnermendesdsg <fagnermendes.dsg@gmail.com>
2025-03-19 10:50:10 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,9 +11,12 @@ printf("Novo commit\n");
     float area1 = 5.6;
     float pib1 = 9.8;
     int pontosturisticos1 = 50;
+    float densidadepopulacional1;
+    float pibpercapita1; 
 
     // Capturando as entradas dos usuários para os atributos
 
+    printf("*** JOGO SUPER TRUNFO *** \n");
     printf("Vamos começar? \n");
 
     printf("\nDigite o Estado da primeira carta: \n");
@@ -37,6 +40,13 @@ printf("Novo commit\n");
     printf("Digite a quantidade de Pontos Turísticos da primeira carta: \n");
     scanf("%d", &pontosturisticos1);
 
+    printf("Densidade Populacional da primeira carta: \n");
+    densidadepopulacional1 = populacao1 / area1;
+
+    printf("PIB per Capita: \n");
+    pibpercapita1 = pib1 / populacao1;
+    
+
     // Exibição das informações formatadas
 
     printf("\nCARTA 1 \n");
@@ -44,9 +54,11 @@ printf("Novo commit\n");
     printf("Código: %s \n", codigo1);
     printf("Cidade: %s \n", nomecidade1);
     printf("População: %d \n", populacao1);
-    printf("Área: %.2f \n", area1);
+    printf("Área: %.2f km² \n", area1);
     printf("PIB: %2.f bilhões de reais \n", pib1);
     printf("Pontos Turísticos: %d \n", pontosturisticos1);
+    printf("Densidade Populacional: %f \n", densidadepopulacional1);
+    printf("PIB per Capita: %f \n", pibpercapita1);
 
      
     // Declaração das variáveis para a segunda carta
@@ -58,6 +70,8 @@ printf("Novo commit\n");
     float area2 = 1.2;
     float pib2 = 2.3;
     int pontosturisticos2 = 30;
+    float densidadepopulacional2;
+    float pibpercapita2;
     
     // Capturando as entradas dos usuários para os atributos
 
@@ -82,6 +96,12 @@ printf("Novo commit\n");
     printf("Digite a quantidade de Pontos Turísticos da segunda carta: \n");
     scanf("%d", &pontosturisticos2);
 
+    printf("Densidade Populacional da primeira carta: \n");
+    densidadepopulacional2 = populacao2 / area2;
+
+    printf("PIB per Capita: \n");
+    pibpercapita2 = pib2 / populacao2;
+
     // Exibição das informações formatadas
 
     printf("\n CARTA 2 \n");
@@ -89,9 +109,11 @@ printf("Novo commit\n");
     printf("Código: %s \n", codigo2);
     printf("Cidade: %s \n", nomecidade2);
     printf("População: %d \n", populacao2);
-    printf("Área: %.2f \n", area2);
+    printf("Área: %.2f km² \n", area2);
     printf("PIB: %2.f bilhões de reais \n", pib2);
     printf("Pontos Turísticos: %d \n", pontosturisticos2);
+    printf("Densidade Populacional: %f \n", densidadepopulacional2);
+    printf("PIB per Capita: %f \n", pibpercapita2);
 
     printf("\nAs Cartas foram registradas com sucesso!\n");
 
